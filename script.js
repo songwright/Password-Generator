1fa223e5acc58497c9eaa1b940038169c662f2b6
Ramon Sanchez <songwright66@gmail.com>
2019-09-19 20:24:29 -0700

    Add special characters array

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Arrays of characters to be used as a source for random
+// character generation.
+let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "=", ":", ";", "'", "<", ">", "?", ",", ".", "/"];
 let numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
