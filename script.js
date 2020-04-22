9dd696872c723efd63730c77fb8bd20b990c760d
Ramon Sanchez <songwright66@gmail.com>
2020-04-21 17:24:31 -0700

    Updated script.js

--- script.js
+++ script.js
@@ -2,89 +2,16 @@
 // character generation.
 let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "=", ":", ";", "'", "<", ">", "?", ",", ".", "/"];
 let numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCaseLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// Characters from the arrays will be joined into a string called "source."
-let source = '';
-
-function setUpPW () {
-
-  // The length of the desired password
-  let inputLength = prompt("How many characters would you like your password to contain?");
-
-  if (inputLength < 8) {
-    alert("Password length must be at least 8 characters.");
-    return;
-  } else if (inputLength > 128) {
-    alert("Password length must less than 129 characters.");
-    return;
-  } else if (isNaN(inputLength)) {
-    alert("Password length must be provided as a number.");
-    return;
-  }
-
-  // Get special characters for source.
-  let special = confirm("Click OK to confirm including special characters.")
-
-  if (special) {
-    source = source + specialCharacters.join('');
-  }
-
-  // Get numeric characters for source.
-  let numeric = confirm("Click OK to confirm including numeric characters.")
-
-  if (numeric) {
-    source = source + numerals.join('');
-  }
-
-  // Get lowercase characters for source.
-  let lower = confirm("Click OK to confirm including lowercase characters.")
-
-  if (lower) {
-    source = source + lowerCase.join('');
-  }
-
-  // Get uppercase characters for source.
-  let upper = confirm("Click OK to confirm including uppercase characters.")
-
-  if (upper) {
-    source = source + upperCase.join('');
-  }
-
-  // Show password.
-  if (source === '') {
-    alert("You must select at least one character type.");
-    return;
-  }
-  return makePassword(inputLength);
-}
-
-// Make password from source string.
-function makePassword(pwLength) {
-  let pw = '';
-  for (let i = 0; i < pwLength; i++) {
-    pw += source[Math.floor(Math.random() * source.length)];
-  }
-  return pw;
-}
-
-// Get references to the #copy and #generate elements
+// Get reference to the Copy to Clipboard button
 let copyBtn = document.querySelector("#copy");
-let generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  let password = setUpPW();
-  let passwordText = document.querySelector("#password");
+// Add event listener to Copy to Clipboard button
+copyBtn.addEventListener("click", copyToClipboard);
 
-  passwordText.value = password;
-  // reset source string
-  source = '';
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
-}
-
+// Copy the password to the clipboard.
 function copyToClipboard() {
   var passwordText = document.querySelector("#password");
 
@@ -96,8 +23,49 @@ function copyToClipboard() {
   );
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function makePassword() {
+  event.preventDefault();
+  
+  let source = "";
+  let pw = "";
+  const characterNumber = document.getElementById("characterNum").value;
 
-// Add event listener to copy button
-copyBtn.addEventListener("click", copyToClipboard);
+  if (!characterNumber) {
+    alert("You must enter a number.");
+    return;
+  }
+
+  // Check for types of characters that will be used for the password.
+
+  if (special.checked) {
+    source = source + specialCharacters.join('');
+  }
+  
+  if (numeral.checked) {
+    source = source + numerals.join('');
+  }
+
+  if (lowercase.checked) {
+    source = source + lowerCaseLetters.join('');
+  }
+  
+  if (uppercase.checked) {
+    source = source + upperCaseLetter.join('');
+  }
+
+  for (let i = 0; i < characterNumber; i++) {
+    pw += source[Math.floor(Math.random() * source.length)];
+    if (source === '') {
+      alert("You must select at least one type of character.");
+      return;
+    }
+  }
+
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = pw;
+
+  // Enable Copy to Clipboard button
+  copyBtn.removeAttribute("disabled");
+  copyBtn.focus();
+}
